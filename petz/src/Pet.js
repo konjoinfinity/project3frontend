0db107c2414cea4e222142ff839a2ee5d9bfc99d
Wesley Scholl <clients@konjoweb.com>
2019-03-19 15:34:41 -0400

    update of pet card

--- petz/src/Pet.js
+++ petz/src/Pet.js
@@ -3,6 +3,14 @@ import "./App.css";
 import petpic from "./cat.jpeg";
 
 class Pet extends Component {
+  lickHandle(event) {
+    event.preventDefault();
+    console.log("Lick");
+  }
+  commentHandle(event) {
+    event.preventDefault();
+    console.log("Comment");
+  }
   render() {
     return (
       <div>
@@ -14,37 +22,43 @@ class Pet extends Component {
                 <span className="card-title">Skywhiskers pet.name</span>
               </div>
               <div className="card-content">
-                <a href="/pet">
-                  <button className="btn orange lighten-2">
-                    Link to Pet Details
-                  </button>
-                </a>
-                <a href="/edit">
-                  <button className="btn orange lighten-2">Edit Pet</button>
-                </a>
                 <p>
                   I am a very simple card. I am good at containing small bits of
                   information. I am convenient because I require little markup
                   to use effectively. pet.description
                 </p>
                 <p>pet.species</p>
-                <button className="btn orange lighten-2"># of pet.licks</button>
-                <div className="card">pet.comments</div>
+                <form onSubmit={this.lickHandle}>
+                  <button className="btn blue lighten-2"># of pet.licks</button>
+                </form>
                 <div className="card">
-                  <form>
-                    <input
-                      id="comment"
-                      name="comment"
-                      type="text"
-                      placeholder="pet.comments"
-                    />
-                    <button className="btn orange lighten-2">Comment</button>
-                  </form>
+                  <div className="card-content">pet.comments</div>
+                </div>
+                <div className="card">
+                  <div className="card-content">
+                    <form onSubmit={this.commentHandle}>
+                      <input
+                        id="comment"
+                        name="comment"
+                        type="text"
+                        placeholder="pet.comments"
+                      />
+                      <button className="btn orange lighten-2">Comment</button>
+                    </form>
+                  </div>
                 </div>
               </div>
               <div className="card-action">
-                <a href="https://twitter.com">pet.sociallink</a>
                 <i className="fab fa-twitter" />
+                <a href="https://twitter.com">pet.sociallink</a>
+                <a href="/edit">
+                  <button className="btn orange lighten-2">Edit Pet</button>
+                </a>
+                <a href="/pet">
+                  <button className="btn blue lighten-2">
+                    Link to Pet Details
+                  </button>
+                </a>
               </div>
             </div>
           </div>
