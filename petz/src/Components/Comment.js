444fdd7dad6bbe82a730c918ae3d8d38efd0abd8
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-20 14:46:59 -0400

    Add name of author to comment card

--- petz/src/Components/Comment.js
+++ petz/src/Components/Comment.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import "./Comment.css";
+import axios from "axios";
 
 class Comment extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: this.props.data._id,
+			pet: ""
+		};
+	}
+	componentDidMount() {
+		axios
+			.get(`http://localhost:3001/api/comments/${this.state.id}/author`)
+			.then(res => this.setState({ pet: res.data }));
+	}
 	render() {
+		console.log(this.state.pet.name);
 		const readDate = new Date(this.props.data.createdAt);
 		return (
 			<div className="comment-card">
 				<p>{this.props.data.message}</p>
-				<p className="date">{readDate.toLocaleString()}</p>
+				<p className="date">
+					Posted by {this.state.pet.name} on {readDate.toLocaleString()}
+				</p>
 			</div>
 		);
 	}
