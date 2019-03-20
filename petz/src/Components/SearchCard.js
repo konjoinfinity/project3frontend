5568c8f0b84f8626fbf1db497df5cb53a2670afd
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-20 10:36:35 -0400

    Style pet search box and add link to pet page

--- petz/src/Components/SearchCard.js
+++ petz/src/Components/SearchCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./SearchCard.css";
 
 class SearchCard extends Component {
@@ -7,7 +8,11 @@ class SearchCard extends Component {
 			<div className="search-card">
 				<img src={this.props.data.profilepicture} alt={this.props.data.name} />
 				<div className="pet-data">
-					<h3>{this.props.data.name}</h3>
+					<h3>
+						<Link to={"/pets/" + this.props.data._id}>
+							{this.props.data.name}
+						</Link>
+					</h3>
 					<p>{this.props.data.description}</p>
 				</div>
 			</div>
