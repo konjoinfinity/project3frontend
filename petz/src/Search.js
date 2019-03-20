82b255ad1b058c148de82a5b81112633fed91910
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-20 09:38:11 -0400

    Implement name search based on Korilla search

--- petz/src/Search.js
+++ petz/src/Search.js
@@ -1,13 +1,61 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Search extends Component {
-  render() {
-    return (
-      <div>
-        <h1>This is the Search hahahahaha!!!</h1>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			input: "*",
+			petList: []
+		};
+		this.updateName = this.updateName.bind(this);
+	}
+
+	componentDidMount() {
+		axios
+			.get("http://localhost:3001/api/pets")
+			.then(res => this.setState({ petList: res.data }))
+			.catch(err => console.log(err));
+	}
+
+	updateName(event) {
+		this.setState({ input: event.target.value }, () => {
+			if (this.state.input === "") {
+				this.setState({ input: "*" });
+			} else {
+				this.setState({ input: this.state.input });
+			}
+		});
+	}
+
+	updateResults() {}
+
+	render() {
+		let results = [];
+
+		this.state.petList
+			.filter(pet =>
+				pet.name.toUpperCase().includes(this.state.input.toUpperCase())
+			)
+			.map(pet => {
+				results.push(pet);
+			});
+
+		return (
+			<div>
+				<h1>Search for a pet</h1>
+				<form>
+					<input
+						type="text"
+						onChange={this.updateName}
+						placeholder="Search pets by name..."
+					/>
+				</form>
+				{this.state.input}
+				{/* {this.state.petList} */}
+			</div>
+		);
+	}
 }
 
 export default Search;
