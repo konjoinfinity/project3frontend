acef5c1a76f4eb7f04b56cd721ab1758adb6b632
Wesley Scholl <clients@konjoweb.com>
2019-03-21 15:21:46 -0400

    updated styling for pet licks index and updated logout for one click on nav

--- petz/src/Search.js
+++ petz/src/Search.js
@@ -5,79 +5,81 @@ import "./Search.css";
 import petsUrl from "./Constants";
 
 class Search extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			input: "*",
-			petList: [],
-			species: ""
-		};
-		this.updateName = this.updateName.bind(this);
-		this.updateSpecies = this.updateSpecies.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "*",
+      petList: [],
+      species: ""
+    };
+    this.updateName = this.updateName.bind(this);
+    this.updateSpecies = this.updateSpecies.bind(this);
+  }
 
-	componentDidMount() {
-		axios
-			.get(petsUrl)
-			.then(res => this.setState({ petList: res.data }))
-			.catch(err => console.log(err));
-	}
+  componentDidMount() {
+    axios
+      .get(petsUrl)
+      .then(res => this.setState({ petList: res.data }))
+      .catch(err => console.log(err));
+  }
 
-	updateName(event) {
-		this.setState({ input: event.target.value }, () => {
-			if (this.state.input === "") {
-				this.setState({ input: "*" });
-			} else {
-				this.setState({ input: this.state.input });
-			}
-		});
-	}
+  updateName(event) {
+    this.setState({ input: event.target.value }, () => {
+      if (this.state.input === "") {
+        this.setState({ input: "*" });
+      } else {
+        this.setState({ input: this.state.input });
+      }
+    });
+  }
 
-	updateSpecies(event) {
-		this.setState({ species: event.target.value }, () => {
-			this.setState({ species: this.state.species });
-		});
-	}
+  updateSpecies(event) {
+    this.setState({ species: event.target.value }, () => {
+      this.setState({ species: this.state.species });
+    });
+  }
 
-	render() {
-		let results = [];
+  render() {
+    let results = [];
 
-		this.state.petList
-			.filter(
-				pet =>
-					pet.name.toUpperCase().includes(this.state.input.toUpperCase()) &&
-					pet.species === this.state.species
-			)
+    this.state.petList
+      .filter(
+        pet =>
+          pet.name.toUpperCase().includes(this.state.input.toUpperCase()) &&
+          pet.species === this.state.species
+      )
 
-			.map(pet => {
-				return results.push(<SearchCard key={pet._id} data={pet} />);
-			});
+      .map(pet => {
+        return results.push(<SearchCard key={pet._id} data={pet} />);
+      });
 
-		return (
-			<div>
-				<div className="search-text input-field">
-					<h2>I'm looking for a</h2>
-					<select value={this.state.species} onChange={this.updateSpecies}>
-						<option value="" disabled>
-							Select a species
-						</option>
-						<option value="Cat">cat</option>
-						<option value="Dog">dog</option>
-					</select>
-				</div>
-				<div className="search-text input-field">
-					<h2>named</h2>
-					<input
-						type="text"
-						onChange={this.updateName}
-						placeholder="Start typing a name"
-					/>
-					<h2>...</h2>
-				</div>
-				{results}
-			</div>
-		);
-	}
+    return (
+      this.props.isLoggedIn === true && (
+        <div>
+          <div className="search-text input-field">
+            <h2>I'm looking for a</h2>
+            <select value={this.state.species} onChange={this.updateSpecies}>
+              <option value="" disabled>
+                Select a species
+              </option>
+              <option value="Cat">cat</option>
+              <option value="Dog">dog</option>
+            </select>
+          </div>
+          <div className="search-text input-field">
+            <h2>named</h2>
+            <input
+              type="text"
+              onChange={this.updateName}
+              placeholder="Start typing a name"
+            />
+            <h2>...</h2>
+          </div>
+          {results}
+        </div>
+      )
+    );
+  }
 }
 
 export default Search;
--- petz/src/Signup.js
+++ petz/src/Signup.js
@@ -3,44 +3,46 @@ import React, { Component } from "react";
 class Signup extends Component {
   render() {
     return (
-      <div className="card m-5">
-        <div className="card-body">
-          <div className="card-content">
-            <h2>Sign Up</h2>
+      this.props.isLoggedIn === false && (
+        <div className="card m-5">
+          <div className="card-body">
+            <div className="card-content">
+              <h2>Sign Up</h2>
 
-            <form>
-              <div>
-                <p>Email</p>
-                <p>
-                  <input
-                    type="text"
-                    name="email"
-                    onChange={this.props.handleInput}
-                  />
-                </p>
-              </div>
+              <form>
+                <div>
+                  <p>Email</p>
+                  <p>
+                    <input
+                      type="text"
+                      name="email"
+                      onChange={this.props.handleInput}
+                    />
+                  </p>
+                </div>
 
-              <div>
-                <p>Password</p>
-                <p>
-                  <input
-                    type="password"
-                    name="password"
-                    onChange={this.props.handleInput}
-                  />
-                </p>
-              </div>
-              <button
-                className="btn orange lighten-1"
-                type="submit"
-                onClick={this.props.handleSignUp}
-              >
-                Sign Up
-              </button>
-            </form>
+                <div>
+                  <p>Password</p>
+                  <p>
+                    <input
+                      type="password"
+                      name="password"
+                      onChange={this.props.handleInput}
+                    />
+                  </p>
+                </div>
+                <button
+                  className="btn orange lighten-1"
+                  type="submit"
+                  onClick={this.props.handleSignUp}
+                >
+                  Sign Up
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      )
     );
   }
 }
